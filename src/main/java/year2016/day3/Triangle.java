bfceafb9efc3cf51e1356d10a42a07cb868af842
Baptiste LE GOUHINEC <legouhinec.b@gmail.com>
2023-04-20 15:22:06 +0200

    2017_3_1 update

--- src/main/java/year2016/day3/Triangle.java
+++ src/main/java/year2016/day3/Triangle.java
@@ -1,43 +1,17 @@
 package year2016.day3;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Triangle {
 
     private int side1;
     private int side2;
     private int side3;
-
-    public Triangle(int side1, int side2, int side3) {
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
-    }
-
-    public Triangle() {
-    }
-
-    public int getSide1() {
-        return side1;
-    }
-
-    public void setSide1(int side1) {
-        this.side1 = side1;
-    }
-
-    public int getSide2() {
-        return side2;
-    }
-
-    public void setSide2(int side2) {
-        this.side2 = side2;
-    }
-
-    public int getSide3() {
-        return side3;
-    }
-
-    public void setSide3(int side3) {
-        this.side3 = side3;
-    }
 
     public boolean isValid(){
         int x = this.side1;
--- src/main/java/year2017/day3/Main.java
+++ src/main/java/year2017/day3/Main.java
@@ -6,23 +6,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //Idée à chaud : On regarde la couronne concerné avec les carrés (dans le coin inférieur droit c'est touujours le carré de la taille de la couronne (1 ,9 ,25 ,etc...))
-        //  comme ca on connait la distance minimale pour y aller et ensuite on a juste a faire des medianes en regardant si notre nombre est plus ou moins grand afin de trouver
-        //  plus en plus précisement son emplacement et donc en rajoutant le nombre de case a parcourir "sur" la couronne
-
-
-        int cursor = 1;
-        int distanceMinCouronne = 0;
+        int curseur = 1;
+        int distanceMinimum = 0;
         int tailleCouronne = 1;
-        int mod = 0;
-        while (cursor < INPUT) {
-            cursor = (int) Math.pow(Math.sqrt(cursor) + 2, 2);
-            distanceMinCouronne += 1;
-            tailleCouronne = 8 * distanceMinCouronne; //Math.sqrt(cursor) - 1) * 4 =:= 8 * distanceMinimum
-            mod = tailleCouronne / 4;
+        int modulo = 0;
+
+        while (curseur < INPUT) {
+            curseur = (int) Math.pow(Math.sqrt(curseur) + 2, 2);
+            distanceMinimum += 1;
+            tailleCouronne = 8 * distanceMinimum; //Math.sqrt(cursor) - 1) * 4 =:= 8 * distanceMinimum
+            modulo = tailleCouronne / 4;
         }
 
-        int tailleChemin = Math.abs((distanceMinCouronne+1)-INPUT%mod) + distanceMinCouronne;
-        System.out.println(tailleChemin);
+        int longueurChemin = Math.abs((distanceMinimum + 1) - INPUT % modulo) + distanceMinimum;
+        System.out.println(longueurChemin);
     }
 }
